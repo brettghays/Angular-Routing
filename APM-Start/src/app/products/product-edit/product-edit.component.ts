0a3868b8ee5fe6d16a1e7098a8a12816a13d53f7
Brett Hays <bhays@simplifile.com>
2019-06-13 11:31:12 -0600

    Module 6

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -5,6 +5,7 @@ import { MessageService } from '../../messages/message.service';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { resolve } from 'dns';
 
 @Component({
   templateUrl: './product-edit.component.html',
@@ -22,12 +23,11 @@ export class ProductEditComponent implements OnInit{
               private router: Router) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      params => {
-        const id = +params.get('id');
-        this.getProduct(id);
-      }
-    )
+    this.route.data.subscribe(data => {
+      const resolvedData: ProductResolved = data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.onProductRetrieved(resolvedData.product);
+    })
   }
 
     getProduct(id: number): void {
