8939bb6538bd5d5268247c2c380de62ea2da81e4
Brett Hays <bhays@simplifile.com>
2019-06-13 11:02:51 -0600

    Module 5

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../../messages/message.service';
 
@@ -9,16 +10,27 @@ import { ProductService } from '../product.service';
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit{
   pageTitle = 'Product Edit';
   errorMessage: string;
 
   product: Product;
 
   constructor(private productService: ProductService,
-              private messageService: MessageService) { }
+              private messageService: MessageService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
-  getProduct(id: number): void {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {
+        const id = +params.get('id');
+        this.getProduct(id);
+      }
+    )
+  }
+
+    getProduct(id: number): void {
     this.productService.getProduct(id)
       .subscribe(
         (product: Product) => this.onProductRetrieved(product),
@@ -81,5 +93,6 @@ export class ProductEditComponent {
     }
 
     // Navigate back to the product list
+    this.router.navigate(['/products']);
   }
 }
